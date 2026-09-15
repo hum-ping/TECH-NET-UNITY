ce4e96de7b56c58b234e70adb6eafb0658a68af2
hum-ping <humpherywamita699@gmail.com>
2026-09-15 19:37:42 +0300

    Rebuild JARVIS Android configuration from scratch

--- jarvis-android/app/build.gradle.kts
+++ jarvis-android/app/build.gradle.kts
@@ -1,11 +1,24 @@
-plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
-android { namespace = "com.technetunity.jarvis"; compileSdk = 35
-    defaultConfig { applicationId = "com.technetunity.jarvis"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+android {
+    namespace = "com.technetunity.jarvis"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.technetunity.jarvis"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "2.0"
+    }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
 }
